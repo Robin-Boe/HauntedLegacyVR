ae9dcec9dcca7474c1f1705ea7d7f09415fc2f46
Daniel <DanielVBagge@gmail.com>
2023-11-30 23:51:50 +0100

    Update footsteps script
    Made it so every step sounds different by randomizing the pitch and volume of each step.

--- VRGame/Assets/Scripts/Footsteps.cs
+++ VRGame/Assets/Scripts/Footsteps.cs
@@ -18,6 +18,8 @@ public class Footsteps : MonoBehaviour
     {
         if(cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false)
         {
+            GetComponent<AudioSource>().volume = Random.Range(0.8f, 1);
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.1f);
             GetComponent<AudioSource>().Play();
         }
     }
