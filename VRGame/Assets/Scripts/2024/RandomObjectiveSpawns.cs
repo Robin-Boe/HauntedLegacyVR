b295e61cc194b14f4a66491380856b1774e975a8
Robin <83815658+Robin-Boe@users.noreply.github.com>
2024-03-05 23:45:55 +0100

    Teleport For Couple and Worker Painting
    --- Collectible ---
    - Added a teleportation to the attic whenever the player picks up the worker painting.
    - Added a teleportation to the basement whenever the player picks up the couple painting.
    - Added a teleportation back to the original spot of where the player picked up the couple / worker painting
    - Added walls with white glow fade in and out, to illustrate a visual hallucination
    - Added walls to the cutscenes to avoid the player walking away. Blocking them in the attic or basement.

--- VRGame/Assets/Scripts/2024/RandomObjectiveSpawns.cs
+++ VRGame/Assets/Scripts/2024/RandomObjectiveSpawns.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.XR.CoreUtils;
 
 public class RandomObjectiveSpawns : MonoBehaviour
 {
@@ -24,6 +25,87 @@ public class RandomObjectiveSpawns : MonoBehaviour
     int randomNumberGhoul;
     int randomNumberYokai;
 
+    // Player Camera
+    public GameObject player;
+
+    // Keep players original position
+    private Vector3 playerOrgPosition;
+
+    // Blockage During Cutscene
+    public GameObject blockageCouple;
+    public GameObject blockageWorker;
+
+    // Checks for testing which painting is active
+    public GameObject Couple0;
+    public GameObject Couple1;
+    public GameObject Couple2;
+    public GameObject Worker0;
+    public GameObject Worker1;
+    public GameObject Worker2;
+
+    // Run the Couple Painting Sequence
+    public void PrepareCouple()
+    {
+        playerOrgPosition = player.transform.position;
+        Invoke("ChangePlayerCouple", 1.5f);
+        Invoke("ReturnPlayerCouple", 29.5f);
+    }
+
+    public void PrepareWorker()
+    {
+        playerOrgPosition = player.transform.position;
+        Invoke("ChangePlayerWorker", 1.5f);
+        Invoke("ReturnPlayerWorker", 29.5f);
+    }
+
+    // Teleport player to basement
+    void ChangePlayerCouple(){
+        player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(-8.697f, -0.2f, -17.489f));
+        blockageCouple.SetActive(true);
+    } 
+
+    // Teleport player to attic
+    void ChangePlayerWorker(){
+        player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(0.563f, 10f, -16.382f));
+        blockageWorker.SetActive(true);
+    } 
+
+    // Teleport player back to original position for worker
+    void ReturnPlayerCouple(){
+        // If position is equally to index 0
+        if (Couple0.activeSelf){
+            player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(-1.102f, -3.9f, -23.182f));
+        }
+        // If position is equally to index 1
+        else if (Couple1.activeSelf){
+            player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(8.705f, 2.078f, -12.271f));
+        }
+        // If position is equally to index 2
+        else if (Couple2.activeSelf){
+            player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(-3.011f, 6.925f, -27.946f));
+        }
+        // If 3, then don't teleport as player already in basement
+        blockageCouple.SetActive(false);
+    }
+
+    // Teleport player back to original position for worker
+    void ReturnPlayerWorker(){
+        // If position is equally to index 0
+        if (Worker0.activeSelf){
+            player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(3.79f, 6.925f, -14.014f));
+        }
+        // If position is equally to index 1
+        else if (Worker1.activeSelf){
+            player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(1.285f, -0.2f, -25.577f));
+        }
+        // If position is equally to index 2
+        else if (Worker2.activeSelf){
+            player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(-11.502f, -0.2f, -30.276f));
+        }
+        // If 3, then don't teleport as player already in attic
+        blockageWorker.SetActive(false);
+    }
+
     void decidePosition(int index, string collectable)
     {
         if (index == 0){
@@ -35,9 +117,11 @@ public class RandomObjectiveSpawns : MonoBehaviour
             }
             else if (collectable == "Couple"){
                 couplePainting.transform.position = new Vector3(-1.468f, -4.502f, -22.244f);
+                Couple0.SetActive(true);
             }
             else if (collectable == "Worker"){
                 workerPainting.transform.position = new Vector3(3.944f, 6.131f, -12.6106f);
+                Worker0.SetActive(true);
             }
             else if (collectable == "Key"){
                 key.transform.position = new Vector3(2.139f, 2.053f, -12.7334f);
@@ -53,9 +137,11 @@ public class RandomObjectiveSpawns : MonoBehaviour
             }
             else if (collectable == "Couple"){
                 couplePainting.transform.position = new Vector3(8.603f, 1.497f, -10.538f);
+                Couple1.SetActive(true);
             }
             else if (collectable == "Worker"){
                 workerPainting.transform.position = new Vector3(0.671f, -0.871f, -21.894f);
+                Worker1.SetActive(true);
             }
             else if (collectable == "Key"){
                 key.transform.position = new Vector3(4.451f, 6.3305f, -17.1548f);
@@ -70,10 +156,12 @@ public class RandomObjectiveSpawns : MonoBehaviour
                 mrsPainting.transform.position = new Vector3(12.386f, 2.472f, -30.56f);
             }
             else if (collectable == "Couple"){
-                couplePainting.transform.position = new Vector3(-6.089f, 9.002f, -17.214f);
+                couplePainting.transform.position = new Vector3(-2.278f, 6.017f, -26.646f);
+                Couple2.SetActive(true);
             }
             else if (collectable == "Worker"){
                 workerPainting.transform.position = new Vector3(-11.88f, -0.884f, -30.087f);
+                Worker2.SetActive(true);
             }
             else if (collectable == "Key"){
                 key.transform.position = new Vector3(9.854f, 6.5377f, -22.5858f);
