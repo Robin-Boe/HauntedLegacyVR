38250ecb9103486772097adbd6ab2969e41ed7f2
Robin <83815658+Robin-Boe@users.noreply.github.com>
2024-03-06 22:07:23 +0100

    Changes in Events
    --- Basement ---
    - Added a girl which spawns whenever the player enters the basement and triggers a specific location
    - Added sound effect "TeeHee... Wanna play?" which is activated with the girl
    
    --- House ---
    - Forced the player into the house whenever they trigger the event. The player is returned outside after the event is over.
    
    --- Other ---
    - Setup the character, script and animation for an event to follow the girl. However, it is yet to be implemented.

--- VRGame/Assets/Scripts/Events/House/HouseOutside.cs
+++ VRGame/Assets/Scripts/Events/House/HouseOutside.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.XR.CoreUtils;
 
 public class HouseOutside : MonoBehaviour
 {
+    // Player
+    public GameObject player;
+
     // Check
     private int checkIfRan = 0;
     private int playerEntered = 0;
-    private float timer = 10.5f;
+    private int checkIfDeactivated = 0;
+    private float timer = 12.5f;
 
     // Fade
     public GameObject whiteFade;
@@ -17,6 +22,17 @@ public class HouseOutside : MonoBehaviour
     public GameObject monster;
     public GameObject solider;
 
+    // Teleport Player
+    void TeleportPlayer(){
+        player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(15.123f, 1.5f, -48.717f));
+    }
+
+    // Activate Event
+    void ActivateHouse(){
+        monster.SetActive(true);
+        solider.SetActive(true);
+    }
+
     // If player enters the box collider
     void OnTriggerEnter (Collider other)
     {
@@ -25,17 +41,19 @@ public class HouseOutside : MonoBehaviour
             playerEntered = 1;
             fadeSound.SetActive(true);
             whiteFade.SetActive(true);
-            monster.SetActive(true);
-            solider.SetActive(true);
+            Invoke("TeleportPlayer", 1);
+            Invoke("ActivateHouse", 2);
         }
     }
 
     void Update()
     {
         // If timer is 0 or less set the monsters to inactive
-        if (timer <= 0.0f){
+        if ((timer <= 0.0f) && (checkIfDeactivated == 0)){
             monster.SetActive(false);
             solider.SetActive(false);
+            player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(11.595f, 1.7f, -43.73f));
+            checkIfDeactivated = 1;
         }
         // If player entered the trigger, run timer
         else if (playerEntered == 1){
