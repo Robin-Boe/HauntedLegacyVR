45b3094cbb6f23e1acae136d27e5c410e886f076
Robin <83815658+Robin-Boe@users.noreply.github.com>
2024-04-11 16:56:01 +0200

    Small Changes to Interfaces
    - Changed the color of the objective "collect the 4 paintings" to be similar to the other objectives in the collectable page
    - Made it so the grab tutorial interface is only available to the user whenever the player has yet to enter the mansion.

--- VRGame/Assets/Scripts/2024/Interfaces/DisableOnThreshold.cs
+++ VRGame/Assets/Scripts/2024/Interfaces/DisableOnThreshold.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class DisableOnThreshold : MonoBehaviour
 {
     public GameObject objectActivate;
+    public GameObject enterMansion;
 
     void OnTriggerEnter (Collider other)
     {
-        if (other.gameObject.name == "XR Origin (XR Rig)"){
+        if ((other.gameObject.name == "XR Origin (XR Rig)") && (!enterMansion.activeSelf)){
             objectActivate.SetActive(true);
         }
     }
 
     void OnTriggerExit (Collider other)
     {
-        if (other.gameObject.name == "XR Origin (XR Rig)"){
+        if ((other.gameObject.name == "XR Origin (XR Rig)") && (!enterMansion.activeSelf)){
             objectActivate.SetActive(false);
         }        
     }
